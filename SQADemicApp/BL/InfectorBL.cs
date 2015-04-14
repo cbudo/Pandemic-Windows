e4158336c6768604bf715716112081351d127c0b
Doolan <doolansr@rose-hulman.edu>
2015-04-14 07:16:04 -0400

    Set up structure and testing structure for Infect

---
+++ SQADemicApp/BL/InfectorBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQADemicApp.BL
+{
+    public class InfectorBL
+    {
+
+        public static List<String> InfectCities()
+        {
+            return new List<string> { "chicago" };
+        }
+
+    }
+}
---
+++ SQADemicAppTest/InfectorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQADemicApp.BL;
+
+namespace SQADemicAppTest
+{
+    [TestClass]
+    public class InfectorTest
+    {
+        [TestMethod]
+        public void TestInfectCities()
+        {
+            InfectorBL.InfectCities();
+        }
+    }
+}
