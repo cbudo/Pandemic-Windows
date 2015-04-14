2f88eed639e9f1aa79bb4e8b3928f690409fb84e
Doolan <doolansr@rose-hulman.edu>
2015-04-14 13:12:01 -0400

    set up getNeighborNames shell

---
+++ SQADemicApp/BL/CityBL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQADemicApp.BL
+{
+    class CityBL
+    {
+        /// <summary>
+        /// Gets the Neighboring Cities
+        /// </summary>
+        /// <param name="cityName"></param>
+        /// <returns>List of Neigboring Cities names</returns>
+        public List<string> getNeighborNames(string cityName)
+        {
+            //switch statment will be best
+            return new List<String> { "city" };
+        }
+
+    }
+}
