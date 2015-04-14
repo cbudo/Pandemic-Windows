76276784f14388e4c2f3b1c7cd6b2aa23b54da05
LadyMopar <richarel@rose-hulman.edu>
2015-04-14 16:20:05 -0400

    beginning hastable
    san fran now has neighbors in a hashtable

--- SQADemicApp/BL/CityBL.cs
+++ SQADemicApp/BL/CityBL.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,28 @@ namespace SQADemicApp.BL
 {
     class CityBL
     {
+
+        Hashtable neighbors = new Hashtable();
+
+        public Hashtable makeNeighborTable()
+        {
+            List<String> sfneighbors = new List<String>();
+            sfneighbors.Add("Chicago");
+            sfneighbors.Add("Los Angeles");
+            sfneighbors.Add("Manila");
+            sfneighbors.Add("Tokyo");
+            neighbors["San Francisco"] = sfneighbors;
+
+            return neighbors;
+        }
+
+
         /// <summary>
         /// Gets the Neighboring Cities
         /// </summary>
         /// <param name="cityName"></param>
         /// <returns>List of Neigboring Cities names</returns>
-        public List<string> getNeighborNames(string cityName)
+        public List<String> getNeighborNames(String cityName)
         {
             //switch statment will be best
             return new List<String> { "city" };
