02a204eb5d48cf4b52dfa0f1dec1e05d9b53735a
Doolan <doolansr@rose-hulman.edu>
2015-05-06 12:35:11 -0400

    Cleaned up Create.cs
    Paired with Emily

--- SQADemicApp/Create.cs
+++ SQADemicApp/Create.cs
@@ -9,74 +9,77 @@ namespace SQADemicApp
 {
     public class Create
     {
+        public static Dictionary<string, City> cityDictionary = new Dictionary<string, City>();
+
         public Create()
         {
             createDictionary();
             setAdjacentCities(new StringReader(Properties.Resources.AdjacentNeighbors));
         }
 
-        //create the blues
-        City sanFrancisco = new City(COLOR.blue, "San Francisco");
-        City chicago = new City(COLOR.blue, "Chicago");
-        City montreal = new City(COLOR.blue, "Montreal");
-        City newYork = new City(COLOR.blue, "New York");
-        City washington = new City(COLOR.blue, "Washington");
-        City atlanta = new City(COLOR.blue, "Atlanta");
-        City london = new City(COLOR.blue, "London");
-        City madrid = new City(COLOR.blue, "Madrid");
-        City paris = new City(COLOR.blue, "Paris");
-        City milan = new City(COLOR.blue, "Milan");
-        City stPetersburg = new City(COLOR.blue, "St Petersburg");
-        City essen = new City(COLOR.blue, "Essen");
-
-        //create the yellows
-        City losAngeles = new City(COLOR.yellow, "Los Angeles");
-        City mexicoCity = new City(COLOR.yellow, "Mexico City");
-        City miami = new City(COLOR.yellow, "Miami");
-        City bogota = new City(COLOR.yellow, "Bogota");
-        City lima = new City(COLOR.yellow, "Lima");
-        City saoPaulo = new City(COLOR.yellow, "Sao Paulo");
-        City buenosAires = new City(COLOR.yellow, "Buenos Aires");
-        City santiago = new City(COLOR.yellow, "Santiago");
-        City lagos = new City(COLOR.yellow, "Lagos");
-        City khartoum = new City(COLOR.yellow, "Khartoum");
-        City kinshasa = new City(COLOR.yellow, "Kinshasa");
-        City johannesburg = new City(COLOR.yellow, "Johannesburg");
-
-        //create the blacks
-        City algiers = new City(COLOR.black, "Algiers");
-        City cairo = new City(COLOR.black, "Cairo");
-        City istanbul = new City(COLOR.black, "Istanbul");
-        City moscow = new City(COLOR.black, "Moscow");
-        City baghdad = new City(COLOR.black, "Baghdad");
-        City riyadh = new City(COLOR.black, "Riyadh");
-        City tehran = new City(COLOR.black, "Tehran");
-        City karachi = new City(COLOR.black, "Karachi");
-        City delhi = new City(COLOR.black, "Delhi");
-        City mumbai = new City(COLOR.black, "Mumbai");
-        City chennai = new City(COLOR.black, "Chennai");
-        City kolkata = new City(COLOR.black, "Kolkata");
-
-        //create the reds
-        City beijing = new City(COLOR.red, "Beijing");
-        City seoul = new City(COLOR.red, "Seoul");
-        City shanghai = new City(COLOR.red, "Shanghai");
-        City tokyo = new City(COLOR.red, "Tokyo");
-        City osaka = new City(COLOR.red, "Osaka");
-        City taipei = new City(COLOR.red, "Taipei");
-        City hongKong = new City(COLOR.red, "Hong Kong");
-        City bangkok = new City(COLOR.red, "Bangkok");
-        City manila = new City(COLOR.red, "Manila");
-        City hoChiMinhCity = new City(COLOR.red, "Ho Chi Minh City");
-        City jakarta = new City(COLOR.red, "Jakarta");
-        City sydney = new City(COLOR.red, "Sydney");
-
-        public static Dictionary<string, City> cityDictionary = new Dictionary<string, City>();
-
         public void createDictionary()
         {
+            #region Createcities
+            //create the blues
+            City sanFrancisco = new City(COLOR.blue, "San Francisco");
+            City chicago = new City(COLOR.blue, "Chicago");
+            City montreal = new City(COLOR.blue, "Montreal");
+            City newYork = new City(COLOR.blue, "New York");
+            City washington = new City(COLOR.blue, "Washington");
+            City atlanta = new City(COLOR.blue, "Atlanta");
+            City london = new City(COLOR.blue, "London");
+            City madrid = new City(COLOR.blue, "Madrid");
+            City paris = new City(COLOR.blue, "Paris");
+            City milan = new City(COLOR.blue, "Milan");
+            City stPetersburg = new City(COLOR.blue, "St Petersburg");
+            City essen = new City(COLOR.blue, "Essen");
+
+            //create the yellows
+            City losAngeles = new City(COLOR.yellow, "Los Angeles");
+            City mexicoCity = new City(COLOR.yellow, "Mexico City");
+            City miami = new City(COLOR.yellow, "Miami");
+            City bogota = new City(COLOR.yellow, "Bogota");
+            City lima = new City(COLOR.yellow, "Lima");
+            City saoPaulo = new City(COLOR.yellow, "Sao Paulo");
+            City buenosAires = new City(COLOR.yellow, "Buenos Aires");
+            City santiago = new City(COLOR.yellow, "Santiago");
+            City lagos = new City(COLOR.yellow, "Lagos");
+            City khartoum = new City(COLOR.yellow, "Khartoum");
+            City kinshasa = new City(COLOR.yellow, "Kinshasa");
+            City johannesburg = new City(COLOR.yellow, "Johannesburg");
+
+            //create the blacks
+            City algiers = new City(COLOR.black, "Algiers");
+            City cairo = new City(COLOR.black, "Cairo");
+            City istanbul = new City(COLOR.black, "Istanbul");
+            City moscow = new City(COLOR.black, "Moscow");
+            City baghdad = new City(COLOR.black, "Baghdad");
+            City riyadh = new City(COLOR.black, "Riyadh");
+            City tehran = new City(COLOR.black, "Tehran");
+            City karachi = new City(COLOR.black, "Karachi");
+            City delhi = new City(COLOR.black, "Delhi");
+            City mumbai = new City(COLOR.black, "Mumbai");
+            City chennai = new City(COLOR.black, "Chennai");
+            City kolkata = new City(COLOR.black, "Kolkata");
+
+            //create the reds
+            City beijing = new City(COLOR.red, "Beijing");
+            City seoul = new City(COLOR.red, "Seoul");
+            City shanghai = new City(COLOR.red, "Shanghai");
+            City tokyo = new City(COLOR.red, "Tokyo");
+            City osaka = new City(COLOR.red, "Osaka");
+            City taipei = new City(COLOR.red, "Taipei");
+            City hongKong = new City(COLOR.red, "Hong Kong");
+            City bangkok = new City(COLOR.red, "Bangkok");
+            City manila = new City(COLOR.red, "Manila");
+            City hoChiMinhCity = new City(COLOR.red, "Ho Chi Minh City");
+            City jakarta = new City(COLOR.red, "Jakarta");
+            City sydney = new City(COLOR.red, "Sydney");
+            #endregion
+
             try
             {
+                #region loadDictionary
                 cityDictionary.Add("San Francisco", sanFrancisco);
                 cityDictionary.Add("Chicago", chicago);
                 cityDictionary.Add("Montreal", montreal);
@@ -125,6 +128,7 @@ namespace SQADemicApp
                 cityDictionary.Add("Ho Chi Minh City", hoChiMinhCity);
                 cityDictionary.Add("Jakarta", jakarta);
                 cityDictionary.Add("Sydney", sydney);
+                #endregion
             }
             catch (Exception e)
             {
