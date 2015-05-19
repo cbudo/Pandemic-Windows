ab932f6f0b225d981108e13cfa1de0afbc7423ef
Doolan <doolansr@rose-hulman.edu>
2015-05-19 01:09:42 -0400

    Airlift DONE

--- SQADemicApp/BL/SpecialEventCardsBL.cs
+++ SQADemicApp/BL/SpecialEventCardsBL.cs
@@ -10,6 +10,7 @@ namespace SQADemicApp.BL
     {
         /// <summary>
         /// Completes the special actions for the Government Grant Card
+        /// Builds a research station in the named city
         /// </summary>
         /// <param name="cityName">City to build a research station</param>
         /// <returns>status flag</returns>
@@ -21,6 +22,13 @@ namespace SQADemicApp.BL
             return true;
         }
 
+        /// <summary>
+        /// Completes the special actios for the AirLift card
+        /// Moves the player to the destiation city if it is a diffrent city
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="destination"></param>
+        /// <returns>status flag</returns>
         public static bool Airlift(Player player, City destination)
         {
             if (player.currentCity.Equals(destination))
