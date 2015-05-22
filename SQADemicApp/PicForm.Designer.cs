d578415a14f315cbb26f2b1157ad5d27d9921b83
budocf <budocf@.rose-hulman.edu>
2015-05-22 01:28:38 -0400

    added picture form

--- SQADemicApp/PicForm.Designer.cs
+++ SQADemicApp/PicForm.Designer.cs
@@ -34,9 +34,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(255, 203);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(227, 385);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(152, 55);
             this.label1.TabIndex = 0;
@@ -44,9 +45,9 @@
             // 
             // PicForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(667, 521);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::SQADemicApp.Properties.Resources.Epidemic;
+            this.ClientSize = new System.Drawing.Size(276, 385);
             this.Controls.Add(this.label1);
             this.Name = "PicForm";
             this.Text = "PicForm";
--- SQADemicApp/PicForm.cs
+++ SQADemicApp/PicForm.cs
@@ -12,9 +12,22 @@ namespace SQADemicApp
 {
     public partial class PicForm : Form
     {
-        public PicForm()
+        public PicForm(bool outbreak, string cityName)
         {
             InitializeComponent();
+            if(outbreak)
+            {
+                this.Text = "Outbreak";
+                this.ClientSize = new System.Drawing.Size(667, 521);
+                this.BackgroundImage = global::SQADemicApp.Properties.Resources.Ebola_Virus_outbreak_2;
+                this.label1.Text = cityName;
+            }
+            else
+            {
+                this.Text = cityName;
+                this.BackgroundImage = global::SQADemicApp.Properties.Resources.Epidemic;
+                this.ClientSize = new System.Drawing.Size(276, 385);
+            }   
         }
     }
 }
