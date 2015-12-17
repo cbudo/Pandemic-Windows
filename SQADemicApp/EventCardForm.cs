1bc17f62ed793fea4f357b42822cbab6f163b3e0
Andrew Ma <maastergotmail@gmail.com>
2015-12-16 22:06:43 -0500

    Added double click event to listbox items
    alternative to "tooltip" option. Done

--- SQADemicApp/EventCardForm.cs
+++ SQADemicApp/EventCardForm.cs
@@ -6,11 +6,19 @@ namespace SQADemicApp
 {
     public partial class EventCardForm : Form
     {
+        private Dictionary<string, string> descDict = new Dictionary<string, string>();
         public EventCardForm()
         {
+            descDict.Add("Airlift", "Move a pawn (yours or another player's) to any city. You must have a player's permission to move their pawn.");
+            descDict.Add("One Quiet Night", "Skip the next infect cities step.");
+            descDict.Add("Resilient Population", "Take a card from the Infection Discard Pile and remove it from the game.");
+            descDict.Add("Government Grant", "Add a Research Station to any city for free.");
+            descDict.Add("Forecast", "Examine the top 6 cards of the Infection Draw Pile, rearrange them in the order of your choice, then place them back on the pile.");
+
             InitializeComponent();
             listBox1.Items.Clear();
             listBox1.Items.AddRange(EventCardNames().ToArray());
+            listBox1.MouseDoubleClick += new MouseEventHandler(Card_Description);
         }
 
         private List<object> EventCardNames()
@@ -64,6 +72,16 @@ namespace SQADemicApp
             catch (NullReferenceException)
             {
                 MessageBox.Show("You must select an event card");
+            }
+        }
+
+        public void Card_Description(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("click");
+            int index = listBox1.IndexFromPoint(e.Location);
+            if(index != System.Windows.Forms.ListBox.NoMatches)
+            {
+                MessageBox.Show(descDict[listBox1.SelectedItem.ToString()]);
             }
         }
     }
